caea50e6439575729eb275ad18b4eb71a21104ef
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-11-23 22:08:41 +0900

    Title: 4번의 크기 비교, Time: 46ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241123/4번의 크기 비교/4-time-comparison.py
@@ -0,0 +1,24 @@
+import sys
+
+a = int(sys.stdin.readline())
+b,c,d,e = map(int,sys.stdin.readline().split())
+
+if a>b:
+    print(1)
+else:
+    print(0)
+
+if a>c:
+    print(1)
+else:
+    print(0)
+
+if a>d:
+    print(1)
+else:
+    print(0)
+
+if a>e:
+    print(1)
+else:
+    print(0)
