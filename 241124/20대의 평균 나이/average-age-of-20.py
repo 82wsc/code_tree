0241241e4de41b963887eebaa2854c7885d8047d
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-11-24 01:57:46 +0900

    Title: 20대의 평균 나이, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 241124/20대의 평균 나이/average-age-of-20.py
+++ 241124/20대의 평균 나이/average-age-of-20.py
@@ -1,16 +1,15 @@
 import sys
 
-
-
 count=0
 total=0
 
 while True:
-    n = int(sys.stdin.readline())
-    if n<30:
+    n = int(sys.stdin.readline().strip())
+
+    if n>=20 and n<30: 
         total+=n
         count+=1
     else:
         break
-                
+    
 print(f"{total/count:.2f}")
