8892746231975010da3f13b4fd81c3117a833046
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-11-24 01:16:19 +0900

    Title: 1까지 나누기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241124/1까지 나누기/divide-by-1.py
+++ 241124/1까지 나누기/divide-by-1.py
@@ -3,11 +3,14 @@ import sys
 n = int(sys.stdin.readline())
 idx=0
 
-if n!=0:
-    while n>0:
-        idx+=1
-        n=n//idx    
-else:
-    idx=0
+while n>=1:
+    idx+=1
+    n=n//idx    
 
 print(idx)
+
+# 8
+# 8//1 = 8
+# 8//2 = 4
+# 4//3 = 1
+# 1//4 = 1
