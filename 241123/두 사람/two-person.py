a06762e92a8dd22d814cdab27a471bd283423ac7
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-11-23 22:13:52 +0900

    Title: 두 사람, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241123/두 사람/two-person.py
+++ 241123/두 사람/two-person.py
@@ -3,8 +3,7 @@ import sys
 a_age , a_sex = map(str, sys.stdin.readline().split())
 b_age , b_sex = map(str, sys.stdin.readline().split())
 
-if int(a_age)>=19 or int(b_age)>=19: 
-    if(a_sex=='M' or b_sex=='M'):
-        print(1)
+if int(a_age)>=19 or int(b_age)>=19 or a_sex=='M' or b_sex=='M': 
+    print(1)
 else:
     print(0)
