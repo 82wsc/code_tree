af556d8c3deb61ba0a4134e651c61439b28d97d5
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-12-04 21:28:56 +0900

    Title: 함수를 이용한 윤년 판별, Time: 40ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241204/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -0,0 +1,12 @@
+import sys
+
+y = int(sys.stdin.readline())
+
+def y_year(n):
+    if n%4!=0:
+        return 'false'
+    if n%100!=0 and n%400==0:
+        return 'false'
+    return 'true'
+
+print(y_year(y))
