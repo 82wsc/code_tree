829b22589643632c612bbb8011a84d4aa880c1e6
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2024-12-06 17:34:45 +0900

    Title: 그 계절, 그 날, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 241206/그 계절, 그 날/that-season-that-day.py
+++ 241206/그 계절, 그 날/that-season-that-day.py
@@ -4,27 +4,38 @@ y,m,d = map(int,sys.stdin.readline().split())
 
 def l_year(x):
     if (x % 4 == 0 and x % 100 != 0) or (x % 400 == 0):
-        return False
-    return True
+        return True
+    return False
 
-def exist_date(y,m,d):
-    if l_year(y)==False and m==2 and d>=29:
-        return -1
-    if (m==4 or m==6 or m==9 or m==11) and d==31:
-        return -1
-    return 1
+def last_day_number(y,m):
+    if l_year(y):
+        if m == 2:
+            return 29
+    else:
+        if m == 2:
+            return 28
+    if m == 4 or m == 6 or m == 9 or m == 11:
+        return 30    
+    return 31
 
-def season(m):
-    if 3<=m<=5:
-        return 'Spring'
-    elif 6<=m<=8:
-        return 'Summer'
-    elif 9<=m<=11:
-        return 'Fall'
-    elif 1<=m<=2 or m==12:
-        return 'Winter'
-    
-if exist_date(y,m,d)==1:
-    print(season(m))
-else:
-    print(exist_date(y,m,d))
+# 윤년이 아닐 때 m월 d일이 존재하는지 여부를 확인하는 함수를 작성합니다.
+def judge_day(y, m, d):
+    if m <= 12 and d <= last_day_number(y,m):
+        return True
+    return False
+
+def season(y,m,d):
+    if judge_day(y,m,d):
+        if 1<=m<=2 or m==12:
+            return 'Winter'
+        elif 3<=m<=5:
+            return 'Spring'
+        elif 6<=m<=8:
+            return 'Summer'
+        elif 9<=m<=11:
+            return 'Fall'
+    else:
+        return -1
+
+print( season(y,m,d))
+
