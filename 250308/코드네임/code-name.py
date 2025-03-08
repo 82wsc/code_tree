8e37b2d77b7ab41729c7c90c2ced9aea975cb716
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-03-08 15:19:11 +0900

    Title: 코드네임, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250308/코드네임/code-name.py
+++ 250308/코드네임/code-name.py
@@ -8,6 +8,12 @@ class agent:
 agents = []
 for _ in range(5):
     name, score = tuple(sys.stdin.readline().split())
+    score = int(score)
     agents.append(agent(name,score))
 
-print(agents[0].name, agents[0].score)
+min_idx = 0
+for i in range(1,5):
+    if agents[min_idx].score > agents[i].score:
+        min_idx = i
+
+print(agents[min_idx].name,agents[min_idx].score)
