a45fd529ac45b601da42c1c296ba92ae21b0a62f
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-03-09 21:46:51 +0900

    Title: 총점 비교, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250309/총점 비교/compare-total-points.py
@@ -0,0 +1,21 @@
+import sys
+
+class student:
+    def __init__(self,name,kor,eng,math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+n = int(sys.stdin.readline())
+
+l = []
+for _ in range(n):
+    name, kor, eng, math = sys.stdin.readline().split()
+    kor,eng,math = int(kor), int(eng),int(math)
+    l.append(student(name,kor,eng,math))
+
+l.sort(key = lambda x: (x.kor + x.eng + x.math))
+
+for i in range(n):
+    print(l[i].name,l[i].kor,l[i].eng,l[i].math)
