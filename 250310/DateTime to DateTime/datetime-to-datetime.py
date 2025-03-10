56c534781717a72e9ac80cd6d1b7b29f69f22091
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-03-10 10:46:19 +0900

    Title: DateTime to DateTime, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250310/DateTime to DateTime/datetime-to-datetime.py
+++ 250310/DateTime to DateTime/datetime-to-datetime.py
@@ -5,7 +5,7 @@ a,b,c = map(int,sys.stdin.readline().split())
 
 def count_mins(x,y,z):
     total_mins = 0
-    if x<11 and y<11 and z<11:
+    if x<=11 and y<=11 and z<11:
         total_mins=1
 
     else:
