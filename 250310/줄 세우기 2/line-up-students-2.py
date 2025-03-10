59f0679c81fa5829dfd6cff1fd1d1347a9197426
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-03-10 09:58:22 +0900

    Title: 줄 세우기 2, Time: 52ms, Memory: 16MB, Status: Passed - CodeTree

--- 250310/줄 세우기 2/line-up-students-2.py
+++ 250310/줄 세우기 2/line-up-students-2.py
@@ -1,19 +1,20 @@
 import sys
 
 class student:
-    def __init__(self,height,weight):
+    def __init__(self,height,weight,idx):
         self.height = height
         self.weight = weight
+        self.idx = idx
 
 n = int(sys.stdin.readline())
 
 
 info = []
-for _ in range(n):
+for i in range(n):
     height,weight = map(int,sys.stdin.readline().split())
-    info.append(student(height,weight))
+    info.append(student(height,weight,i+1))
 
 info.sort(key = lambda x: (x.height, -x.weight))
 
 for i in range(n):
-    print(info[i].height,info[i].weight,i+1)
+    print(info[i].height,info[i].weight,info[i].idx)
