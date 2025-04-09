74af1aec45692c58d07e293dfe358adbdd30ef4f
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-04-09 14:34:19 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 208ms, Memory: 48MB, Status: Passed - Codetree

--- 250409/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
+++ 250409/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.py
@@ -2,9 +2,39 @@ import sys
 
 n = 3
 
-segments = [
+segments =[
     tuple(map(int,sys.stdin.readline().split()))
-    for _ in range(n)
+    for _ in range(3)
 ]
 
-print(segments)
+
+max_r = 2000
+offset = 1000
+
+l = [[0 for _ in range(max_r+1)] for _ in range(max_r+1)]
+
+for x1,y1,x2,y2 in segments[:2]:
+    for i in range(offset+x1,offset+x2):
+        for j in range(offset+y1,offset+y2):
+            l[i][j]+=1
+
+area = 0
+
+for i in range(max_r+1):
+    for j in range(max_r+1):
+        if l[i][j]==1:
+            area +=1
+
+area_m =0
+
+x1, y1, x2, y2 = segments[2]
+for i in range(offset+x1,offset+x2):
+    for j in range(offset+y1,offset+y2):
+        l[i][j]+=1
+
+for i in range(max_r+1):
+    for j in range(max_r+1):
+        if l[i][j]==2:
+            area_m +=1
+
+print(area-area_m)
