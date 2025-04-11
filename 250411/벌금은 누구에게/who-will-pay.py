fb35fefca5fd130c0fe52bc68fd94397c20cd9cc
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-04-11 16:37:09 +0900

    Title: 벌금은 누구에게, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/벌금은 누구에게/who-will-pay.py
+++ 250411/벌금은 누구에게/who-will-pay.py
@@ -4,10 +4,14 @@ n, m, k = map(int,sys.stdin.readline().split())
 
 l = [0 for _ in range(n)]
 
+
+ans = -1
 for _ in range(m):
     idx = int(sys.stdin.readline())
     l[idx-1] += 1
 
     if max(l)>=k:
-        print(l.index(max(l))+1)
+        ans = l.index(max(l))+1
         break
+        
+print(ans)
