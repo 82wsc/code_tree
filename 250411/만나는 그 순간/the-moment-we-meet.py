63218cafd735c6ddcd0eb8cb8efaeee5b0499d1d
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-04-11 16:06:54 +0900

    Title: 만나는 그 순간, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250411/만나는 그 순간/the-moment-we-meet.py
+++ 250411/만나는 그 순간/the-moment-we-meet.py
@@ -7,8 +7,6 @@ max_r = 1000
 l_a = [0 for _ in range(max_r)]
 l_b = [0 for _ in range(max_r)]
 
-print(l)
-
 segments_a = [
     tuple(sys.stdin.readline().split())
     for _ in range(n)
@@ -19,30 +17,30 @@ segments_b = [
     for _ in range(m)
 ]
 
-for d,i in segments_a:
-    i = int(i)
-    if d=='R':
-        for x in range(1,i):
-            l_a[x] = l_a[x-1] + 1
-            
-    if d=='L':
-        for x in range(1,i):
-            l_a[x] = l_a[x-1] - 1
 
-for d,i in segments_b:
-    i = int(i)
-    if d=='R':
-        for x in range(1,i):
-            l_b[x] = l_b[x-1] + 1
-            
-    if d=='L':
-        for x in range(1,i):
-            l_b[x] = l_b[x-1] - 1
+def func(segments,l):
+    idx = 1
+    for d,i in segments:
+        i = int(i)
+        
+        if d=='R':
+            for x in range(idx,idx+i):
+                l[x] = l[x-1] + 1
+            idx += i
+                
+        if d=='L':
+            for x in range(idx,idx+i):
+                l[x] = l[x-1] - 1
+            idx += i
+    return l
 
-print(l_a)
-print(l_b)
+func(segments_a,l_a)
+func(segments_b,l_b)
 
+for i in range(1,max_r):
+    if l_a[i]==l_b[i]:
+        print(i)
+        break
+else:
+    print(-1)
 
-
-
-    
