b2bfb488396e35788115dd713d58e2c23243442f
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-06-26 16:30:37 +0900

    Title: 최고의 13위치, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/최고의 13위치/best-place-of-13.py
+++ 250626/최고의 13위치/best-place-of-13.py
@@ -4,6 +4,13 @@ n = int(sys.stdin.readline())
 
 arr = [
     list(map(int,sys.stdin.readline().split()))
+    for _ in range(n)
 ]
 
-print(arr)
+max_cnt = 0
+
+for i in range(n):
+    for j in range(n-2):
+        max_cnt = max(max_cnt, arr[i][j]+arr[i][j+1]+arr[i][j+2])
+
+print(max_cnt)
