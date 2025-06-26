2a3b1225a15bce3c31a2fd0db775f61f7440a888
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-06-26 15:38:36 +0900

    Title: 모이자, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/모이자/gather.py
+++ 250626/모이자/gather.py
@@ -1,6 +1,17 @@
 import sys
 
 n = int(sys.stdin.readline())
-a = list(map(int,sys.stdin.readline()))
+a = list(map(int,sys.stdin.readline().split()))
 
-print(a)
+min_sum = sys.maxsize
+
+for i in range(n):
+    
+    sum_diff = 0
+    
+    for j in range(n):
+        sum_diff += a[j] * abs(j-i)
+
+    min_sum = min(min_sum, sum_diff)
+
+print(min_sum)
