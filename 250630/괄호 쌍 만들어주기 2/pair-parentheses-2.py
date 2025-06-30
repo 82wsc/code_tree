c8412f32604ab3c90a8447cd8e74f313951dd061
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-06-30 17:16:47 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250630/괄호 쌍 만들어주기 2/pair-parentheses-2.py
+++ 250630/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -5,7 +5,7 @@ n= len(s)
 
 cnt=0
 for i in range(n-1):
-    for j in range(n-1):
+    for j in range(i,n-1):
         if s[i]=='('and s[i+1]=='(' and s[j]==')' and s[j+1]==')':
             cnt+=1
 
