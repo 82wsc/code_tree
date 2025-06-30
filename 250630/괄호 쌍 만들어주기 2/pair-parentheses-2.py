8d3d85a58152ac055cf8419d0f0fa74a4eb3a1b2
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-06-30 17:10:39 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250630/괄호 쌍 만들어주기 2/pair-parentheses-2.py
@@ -0,0 +1,12 @@
+import sys
+
+s = list(sys.stdin.readline().strip())
+n= len(s)
+
+cnt=0
+for i in range(n-1):
+    for j in range(n-1):
+        if s[i]=='('and s[i+1]=='(' and s[j]==')' and s[j+1]==')':
+            cnt+=1
+
+print(cnt)
