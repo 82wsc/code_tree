64e1599a6672bf166ccfd2c44828f1753cdf42cf
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-07-15 19:17:07 +0900

    Title: 최고의 13위치 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250715/최고의 13위치 2/best-place-of-13-2.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+l = [
+    list(map(int,sys.stdin.readline().split()))
+    for _ in range(n)
+]
+
+sum_max = -sys.maxsize
+
+for i in range(n-1):
+    for j in range(n-2):
+        sum_max = max(sum_max,sum(l[i][j:j+3])+sum(l[i+1][j:j+3]))
+
+if n>=6:
+    for i in range(n):
+         for j in range(n-5):  
+            sum_max = max(sum_max, sum(l[i][j:j+3])+sum(l[i][j+3:j+6]))
+
+print(sum_max)
