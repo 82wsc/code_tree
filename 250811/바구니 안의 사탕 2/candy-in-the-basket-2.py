000b49e9c0469cf93917f4961dd7df7a5846d4ae
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-08-11 17:40:23 +0900

    Title: 바구니 안의 사탕 2, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250811/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250811/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -2,7 +2,7 @@ import sys
 
 n, k = map(int,sys.stdin.readline().split())
 
-arr = [0 for _ in range(100)]
+arr = [0 for _ in range(400)]
 
 for i in range(n):
     y,x = map(int,sys.stdin.readline().split())
@@ -10,10 +10,11 @@ for i in range(n):
     arr[x-1] += y
 
 ans = -sys.maxsize
-for i in range(100-(k*2)):
+for i in range(400-(k*2)):
     sum_arr = 0
     for j in range(i,i+(2*k)+1):
         sum_arr += arr[j]
+
     ans = max(ans,sum_arr)
 
 print(ans)
