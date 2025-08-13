3f2c8269b936627a74dccbae563026377cd1a8d8
Won SeungJo <140129806+82wsc@users.noreply.github.com>
2025-08-13 17:40:42 +0900

    Title: 개발팀의 능력, Time: 77ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250813/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 250813/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -7,9 +7,12 @@ def get_diff(i,j,k,l):
     sum2 = arr[k]+arr[l]
     sum3 = sum(arr)-sum1-sum2
 
+    if sum1 == sum2 or sum1 == sum3 or sum2 == sum3:
+        return None
     return max(sum1,sum2,sum3)-min(sum1,sum2,sum3)
 
 ans = sys.maxsize
+find = False
 
 for i in range(len(arr)):
     for j in range(i+1,len(arr)):
@@ -19,10 +22,14 @@ for i in range(len(arr)):
             for l in range(k+1,len(arr)):
                 if l==i or l==j:
                     continue
-            
-                if get_diff(i,j,k,l)==0:
+
+                diff = get_diff(i,j,k,l)
+
+                if get_diff(i,j,k,l)==0 or  get_diff(i,j,k,l) is None:
                     continue
+                
+                if diff<ans:
+                    ans = diff
+                    found = True
 
-                ans = min(ans,get_diff(i,j,k,l))
-
-print(ans)
+print(ans if found else -1)
